87c4f620b174f143b59e711199d1ddde36268a13
dpdahal <dp48dahal@gmail.com>
2020-12-21 11:05:37 +0545

    Introduction

---
+++ c introduction/introduction.c
@@ -0,0 +1,22 @@
+#include<stdio.h> // global library files
+/*
+auto, break, case,char, const,
+continue, default,do,double, else,
+enum,extern,float, for,goto,if,int,
+long, register,return,short,signed,sizeof,static,
+switch, typedef,union, struct,unsigned, volatile,
+while.
+*/
+
+
+int main(){
+
+    // type variable name
+//   int a=10; // 10 value
+//   int b=20;
+//   printf("A: %d B: %d",a,b);
+    //printf("Hello C Programming");
+    return 0;
+}
+
+
